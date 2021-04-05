6a762904a35f34c50ca6e79011d4a25c29da1a5d
amitsinghhacks <as216889@gmail.com>
2021-04-05 14:16:38 +0530

    adding sending mails part

---
+++ Email Sending.py
@@ -0,0 +1,19 @@
+
+import smtplib
+
+sender_email = "aniketsingh9920932616@gmail.com"
+rec_email = "aniketsingh9920932616@gmail.com"
+password = input(str("Please enter your password : "))
+message = "Hey, this was sent using python"
+
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+
+n=int(input("enter 2+3"))
+if n==5:
+    server.login(sender_email, password)
+    print("Login success")
+    server.sendmail(sender_email, rec_email, message)
+    print("Email has been sent to ", rec_email)
+else:
+    print("noob")
